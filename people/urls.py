45740a7516e0724e767679d3464cd47da868d57d
William <williammaucla@gmail.com>
2020-04-15 19:08:23 -0400

    feat: schema looks available

--- people/urls.py
+++ people/urls.py
@@ -4,12 +4,35 @@ from people.views import PersonListView, PersonCreateView, PersonUpdateView, Per
 from django.urls import include, path
 from rest_framework import routers
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+
+
 router = routers.DefaultRouter()
-router.register(r'my-people', PersonSerializerView, basename='Person')
+router.register(r'person', PersonSerializerView, basename='Person')
+
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path(r'swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path(r'swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path(r'redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('people-serializer/', include(router.urls)),
     path('people/', PersonListView.as_view(), name='person_list'),
-    path('add/', PersonCreateView.as_view(), name='person_add'),
-    path('<int:pk>/edit/', PersonUpdateView.as_view(), name='person_edit'),
+    path('people/add/', PersonCreateView.as_view(), name='person_add'),
+    path('people/<int:pk>/edit/', PersonUpdateView.as_view(), name='person_edit'),
 ]
--- project_config/settings.py
+++ project_config/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'webpage.apps.WebpageConfig',
     'people',
     'rest_framework',
+    'drf_yasg'
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,25 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+SWAGGER_SETTINGS = {
+    'USE_SESSION_AUTH': False,
+    'SECURITY_DEFINITIONS': {
+        'Your App API - Swagger': {
+            'type': 'oauth2',
+            'authorizationUrl': '/yourapp/o/authorize',
+            'tokenUrl': '/yourapp/o/token/',
+            'flow': 'accessCode',
+            'scopes': {
+                'read groups': 'read groups',
+            }
+        }
+    },
+    'OAUTH2_REDIRECT_URL': 'http://localhost/static/drf-yasg/swagger-ui-dist/oauth2-redirect.html',
+    'OAUTH2_CONFIG': {
+        'clientId': 'yourAppClientId',
+        'clientSecret': 'yourAppClientSecret',
+        'appName': 'your application name'
+
+    },
+}
